f03d157fca1d3f74e0ce168e31fdee5f8a7332d8
RealSebasTheFish <sebastianq04@gmail.com>
2025-03-31 10:10:30 -0400

    Added admin dashboard

--- frontend/app/logged_in/page.js
+++ frontend/app/logged_in/page.js
@@ -8,17 +8,34 @@ export default function LoggedInPage() {
   var token = null;
   // Fetch user data on component mount
   useEffect(() => {
-    console.log("RUNNING INITIALIZATION");
-    token = localStorage.getItem('rrtaxtoken');
-    if (!token) {
-      console.error("Token is missing");
-      router.push('/');
-      return;
+    async function init() {
+      console.log("RUNNING INITIALIZATION");
+      token = localStorage.getItem('rrtaxtoken');
+      if (!token) {
+        console.error("Token is missing");
+        router.push('/');
+      }
+      else{
+        const response = await fetch(`http://localhost:3001/user/?token=${token}`);
+        const userInfo = await response.json();
+        if (!userInfo) {
+          console.error("There was an error getting the user data.");
+        }
+        else {
+          const userData = await JSON.parse(userInfo.user.data);
+          if (userData.type == "admin") {
+            // Redirect to the default tab (dashboard) when accessing /logged_in
+            router.push('/logged_in/admin');
+          }
+          else {
+            console.log("NON ADMIN");
+            router.push('/logged_in/dashboard');
+          }
+        }
+        
+      }
     }
-    else{
-       // Redirect to the default tab (dashboard) when accessing /logged_in
-    router.push('/logged_in/dashboard');
-    }
+    init();
 
     // Get user data including uid from token
     //fetchUserData();
